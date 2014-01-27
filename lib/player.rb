3d6bdfdecad7f88a7dd6e7f6ac4a37ff22919d07
Abraham Thomas <dysruptmedia@gmail.com>
2014-01-27 13:37:11 +0000

    Added ability to check if the player still has ships floating

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,20 @@
 class Player
 
 attr_reader :name
+attr_accessor :board
 
   def initialize(name)
     @name = name
+    @board = Board.new(self)
   end
 
+  def has_ships_still_floating?
+    rows = board.rows
+    rows_with_ships = rows.select { |a| a.include?("s") }.length
+    rows_with_ships != 0 
+  end
 
-
+  # def shoot(at_coordinates, opponent_board)
+      #  board.register_
+  # end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,14 +3,44 @@ require "./lib/player"
 describe Player do
 
   let(:player) {Player.new("name")}
+  #let(:board) {Board.new}
+
+  def clear_rows # This is terrible. Remove ASAP
+    player.board.set_value_at_coords(" ", "H1")
+    player.board.set_value_at_coords(" ", "I1")
+    player.board.set_value_at_coords(" ", "J1")
+    player.board.set_value_at_coords(" ", "C4")
+    player.board.set_value_at_coords(" ", "C5")
+    player.board.set_value_at_coords(" ", "C6")
+    player.board.set_value_at_coords(" ", "A9")
+    player.board.set_value_at_coords(" ", "A10")
+  end
 
   it "there should be a player" do
     expect(player.name).to eq("name") 
   end
 
 
-  
+  it "should have a board" do
+    expect(player.board).to be_true
+  end
 
+  it "should tell us if opponent still has ships floating initially" do
+    # expect the players board to have ships floating
+    expect(player).to have_ships_still_floating
+  end
 
+  it "should tell us if there are no ships floating" do
+    clear_rows
+    expect(player).not_to have_ships_still_floating
+  end
 
 end
+
+
+
+ # it "should be able to place a ship horizontally" do
+   #    ships = double :ships
+   #    ships.place_ships([a..l])
+   #    # expect(ships).to   
+   #  end

dbdddadde00d288eae11872800d6a2a7ceffc9f7
James Hunter <james.hunter@me.com>
2014-01-27 12:05:11 +0000

    Added ability to check and set values at particular coordinates.

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,38 @@
 class Board
 	def initialize player
 		@player = player
-		@rows = Array.new(10) { Array.new(10) { '' } }
+		#@rows = Array.new(10) { Array.new(10) { '' } }
+		@rows = [
+			       [" "," "," "," "," "," "," ","s","s","s"],
+			       [" "," "," "," "," "," "," "," "," "," "],
+			       [" "," "," "," "," "," "," "," "," "," "],
+			       [" "," ","s"," "," "," "," "," "," "," "],
+			       [" "," ","s"," "," "," "," "," "," "," "],
+			       [" "," ","s"," "," "," "," "," "," "," "],
+			       [" "," "," "," "," "," "," "," "," "," "],
+			       [" "," "," "," "," "," "," "," "," "," "],
+			       ["s"," "," "," "," "," "," "," "," "," "],
+			       ["s"," "," "," "," "," "," "," "," "," "]
+			     ]
+		@x_axis = { "A" => 0, "B" => 1, "C" => 2, "D" => 3,"E" => 4, "F" => 5,
+							"G" => 6, "H" => 7, "I" => 8, "J" => 9
+						}
+	end
+
+	def check_coords coords
+		x, y = convert_coords(coords)
+		@rows[y][x]
+	end
+
+	def set_value_at_coords value, coords
+		x, y = convert_coords(coords)
+		@rows[y][x] = "o"
+	end
+
+	def convert_coords coords
+		x = @x_axis[coords[0]]
+		y = coords.slice(1..-1).to_i - 1
+		[x, y]
 	end
 
 	def owner
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,30 @@
 require 'board'
 
+
 describe Board do 
+let(:player) {double :player, name: "Fred"}
+let(:board) {Board.new(player)}
+
 	it "should know who owns it" do
-		player = double :player, name: "Fred"
-		board = Board.new(player)
 		expect(board.owner).to eq("Fred")
 	end
 
 	it "should return a representation of the board" do
-		player = double :player, name: "Fred"
-		board = Board.new(player)
 		expect(board.rows.length).to eq(10)
 		board.rows.each { |a| expect(a.length).to eq(10) }
 	end
+
+	it "should know what's at any co-ordinates" do
+			expect(board.check_coords("A7")).to eq(" ")
+			expect(board.check_coords("A8")).to eq(" ")
+			expect(board.check_coords("A9")).to eq("s")
+			expect(board.check_coords("A10")).to eq("s")
+	end
+
+  it "should be able to set the value at particular co-ordinates" do
+  	expect(board.check_coords("A1")).to eq(" ")
+  	board.set_value_at_coords("o", "A1")
+  	expect(board.check_coords("A1")).to eq("o")
+  end
+
 end
